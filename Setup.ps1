89ec820f0b9992c4eb73871e73b3153ec231932d
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-05-17 20:09:24 -0400

    Begin Development

---
+++ Setup.ps1
@@ -0,0 +1,26 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install GoogleChrome -y
+
+choco install putty -y
+
+choco install winscp -y
+
+choco install advanced-ipscanner -y
+
+choco install git -y
+
+choco install vscode -y
+
+choco install ringcentral-classic -y
+
+choco install vim -y
+
+choco install sysinternals -y
+
+choco install drawio -y
+
+choco install gimp -y
+
+choco install powertoys -y
