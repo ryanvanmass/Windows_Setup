2442e120a4167207ce2f2ebea813e83c42bddd63
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-07-17 10:50:24 -0400

    Fix URL parametor error

--- Setup.ps1
+++ Setup.ps1
@@ -2,8 +2,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Set up Drop Down Terminal
-Invoke-WebRequest -URL "http://edmi.vanmassenhoven.com/index.php/s/m2GrjeLPfcfqPrr/download/terminal.ico" -OutFile "C:\Users\ryan\Pictures\terminal.ico"
-Invoke-WebRequest -URL "http://edmi.vanmassenhoven.com/index.php/s/ypT9jtdePX5dyep/download/Dropdown%20Terminal.lnk" -OutFile "C:\Users\ryan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Dropdown Terminal.lnk"
+Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/m2GrjeLPfcfqPrr/download/terminal.ico -OutFile "C:\Users\ryan\Pictures\terminal.ico"
+Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/ypT9jtdePX5dyep/download/Dropdown%20Terminal.lnk -OutFile "C:\Users\ryan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Dropdown Terminal.lnk"
 
 
 # Install Software
