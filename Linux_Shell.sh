068987a65dd14517f59719269354a41ce966ea35
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-07-26 20:33:36 -0400

    Finish building script

--- Linux_Shell.sh
+++ Linux_Shell.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+wget http://edmi.vanmassenhoven.com/index.php/s/aNncBdbZf8fzmDc/download/Theme.omp.json
 sudo chmod +x /usr/local/bin/oh-my-posh
+
+eval "$(oh-my-posh init bash --config ~/Theme.omp.json)"
