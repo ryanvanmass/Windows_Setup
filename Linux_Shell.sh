e689e5cc5cd689432da00b8f35cf28f6f5f9765b
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-07-26 20:42:10 -0400

    Update the Script to fix bugs

--- Linux_Shell.sh
+++ Linux_Shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-wget http://edmi.vanmassenhoven.com/index.php/s/aNncBdbZf8fzmDc/download/Theme.omp.json
+wget http://edmi.vanmassenhoven.com/index.php/s/aNncBdbZf8fzmDc/download/Theme.omp.json -O /home/ryan/Theme.omp.json
 sudo chmod +x /usr/local/bin/oh-my-posh
 
-eval "$(oh-my-posh init bash --config ~/Theme.omp.json)"
+echo 'eval "$(oh-my-posh init bash --config ~/Theme.omp.json)"' >> /home/ryan/.bashrc
