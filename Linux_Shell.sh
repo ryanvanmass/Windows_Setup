44a7c24c9d9a103e33fb8f821585ecd93296cd72
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-07-26 21:04:30 -0400

    Fix Typo

--- Linux_Shell.sh
+++ Linux_Shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download Oh-My-Posh
-sudo wget -no-check-certificate https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo wget --no-check-certificate https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
 # Download Theme
