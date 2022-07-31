9be595c7f9085560f45719052038fde4f5524b8e
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-07-31 19:02:28 -0400

    COmment Cleanup and reformmatting

--- Setup.ps1
+++ Setup.ps1
@@ -2,25 +2,29 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 ### Configure Terminal and Prompt ###
-# Set up Drop Down Terminal
+## Set up Drop Down Terminal ##
 Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/m2GrjeLPfcfqPrr/download/terminal.ico -OutFile "C:\Users\ryan\Pictures\terminal.ico"
 Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/bcrMyjQZg4yDiAH/download/Dropdown%20Terminal.lnk -OutFile "C:\Users\ryan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Dropdown Terminal.lnk"
 
-# Configure Oh My Posh - Powershell
+## Configure Powershell Prompt ##
+# Install Oh-My-Posh
 mkdir C:\Users\ryan\Documents\WindowsPowerShell\
 Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/aNncBdbZf8fzmDc/download/Theme.omp.json -OutFile C:\Users\ryan\Documents\WindowsPowerShell\Theme.omp.json
-Write-Output 'oh-my-posh init pwsh --config "C:\Users\ryan\Documents\WindowsPowerShell\Theme.omp.json" | Invoke-Expression' >> C:\Users\ryan\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
 choco install oh-my-posh -y
 choco install firacodenf -y
 Set-ExecutionPolicy Unrestricted
 
-# Configure Windows Terminal
+# Configure Ppowershell Profile
+# Write-Output 'oh-my-posh init pwsh --config "C:\Users\ryan\Documents\WindowsPowerShell\Theme.omp.json" | Invoke-Expression' >> C:\Users\ryan\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+
+
+## Configure Windows Terminal ##
 mkdir -p C:\Users\ryan\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 Invoke-WebRequest http://edmi.vanmassenhoven.com/index.php/s/ywxdD6CFZFDB249/download/settings.json -OutFile C:\Users\ryan\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 choco install microsoft-windows-terminal -y
 
-# WSL Configuration
+## WSL Configuration ##
 $WSL_Check=Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Select -ExpandProperty State # Caches the variable if WSL is enabled or not
 
 if($WSL_Check -eq "Enabled"){
