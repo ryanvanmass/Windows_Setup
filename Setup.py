bebea04034fff8331eabfd8719bb06b7452fcc16
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-03-31 22:07:21 -0400

    Remove Google Drive

--- Setup.py
+++ Setup.py
@@ -63,7 +63,7 @@ else:
 
 ## Choco Testing
 
-Packages = ["putty", "winscp", "advanced-ip-scanner", "git", "vscode", "vim", "drawio", "PowerToys", "googledrive", "vlc", "gsudo", "teamviewer"]
+Packages = ["putty", "winscp", "advanced-ip-scanner", "git", "vscode", "vim", "drawio", "PowerToys", "vlc", "gsudo", "teamviewer"]
 
 for i in range(len(Packages)):
     temp = "choco install " + Packages[i] + " -y"
