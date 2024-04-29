a073c2023230a7ecba65431c43cb7103dfbee61d
Ryan Van Massenhoven <65045018+ryanvanmass@users.noreply.github.com>
2024-04-28 20:26:39 -0400

    Disable MremoteNG install

--- Setup.py
+++ Setup.py
@@ -82,8 +82,8 @@ system("powershell.exe Invoke-Webrequest 'https://edmi.app/index.php/s/Mxp4Yrtt3
 
 
 # Install MremoteNG
-request.urlretrieve('https://github.com/mRemoteNG/mRemoteNG/releases/download/2023.03.03-v1.77.3-nb/mRemoteNG-Installer-1.77.3.nb-1784.msi, /Users/ryan/Download/mRemoteNG.msi')
-system("/Users/ryan/Download/mRemoteNG.msi /quiet")
+# request.urlretrieve('https://github.com/mRemoteNG/mRemoteNG/releases/download/2023.03.03-v1.77.3-nb/mRemoteNG-Installer-1.77.3.nb-1784.msi, /Users/ryan/Download/mRemoteNG.msi')
+# system("/Users/ryan/Download/mRemoteNG.msi /quiet")
 
 
 
