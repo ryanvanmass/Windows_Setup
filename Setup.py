d56737bfef4a1bc12bdfdbdcb9eb49814d31126b
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-11-30 18:11:31 -0500

    Update ClickPaste Version

--- Setup.py
+++ Setup.py
@@ -76,7 +76,7 @@ system("winget install 9P7KNL5RWT25 --accept-package-agreements")
 # system("Enable-WindowsOptionalFeature -FeatureName ClientForNFS-Infrastructure -Online -All")
 
 # Install Clickpaste
-request.urlretrieve('https://github.com/Collective-Software/ClickPaste/releases/download/v1.0.1/ClickPaste_v1.0.1.zip', '/Users/ryan/Downloads/ClickPaste.zip')
+request.urlretrieve('https://github.com/Collective-Software/ClickPaste/releases/download/v1.3.0/ClickPaste_v1.3.0.zip', '/Users/ryan/Downloads/ClickPaste.zip')
 unpack_archive('/Users/ryan/Downloads/Clickpaste.zip','/Program Files/ClickPaste/')
 system("powershell.exe Invoke-Webrequest 'https://edmi.app/index.php/s/Mxp4Yrtt3EtwYiG/download/ClickPaste.exe.lnk' -Outfile 'C:\\Users\\ryan\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\ClickPaste.exe.lnk'")
 
