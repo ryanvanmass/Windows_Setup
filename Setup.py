4c8e2f5c85f9700c14a0327ccea2ced90cd83d7e
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2025-04-02 21:20:03 -0400

    Add Winget Auto Update

--- Setup.py
+++ Setup.py
@@ -54,7 +54,7 @@ else:
 
 ### Install Software ###
 ## 9P7KNL5RWT25 = Sysinternals
-Packages = ["putty.putty", "winscp.winscp", "Famatech.AdvancedIPScanner", "git.git", "vim.vim", "JGraph.draw", "Microsoft.PowerToys", "videolan.vlc", "Google.chrome", "Rufus.Rufus", "xpipe-io.xpipe", "joplin.joplin", "tailscale.tailscale", "nextcloud.nextclouddesktop", "JanDeDobbeleer.OhMyPosh"]
+Packages = ["putty.putty", "winscp.winscp", "Famatech.AdvancedIPScanner", "git.git", "vim.vim", "JGraph.draw", "Microsoft.PowerToys", "videolan.vlc", "Google.chrome", "Rufus.Rufus", "xpipe-io.xpipe", "joplin.joplin", "tailscale.tailscale", "nextcloud.nextclouddesktop", "JanDeDobbeleer.OhMyPosh", "Romanitho.Winget-AutoUpdate"]
 
 for i in range(len(Packages)):
    temp = "winget install " + Packages[i] + " --accept-package-agreements --accept-source-agreements"
