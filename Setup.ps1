9b2e8ed0b6fea1776bfc955d902f8e6fab0a9205
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2025-04-05 21:30:23 -0400

    Create user Dir variable to allow for easy updating of user directory base

--- Setup.ps1
+++ Setup.ps1
@@ -4,6 +4,8 @@ $fontFolderPath = "C:\Users\ryan\Downloads\fonts"
 
 $Packages = @("putty.putty", "winscp.winscp", "Famatech.AdvancedIPScanner", "git.git", "vim.vim", "JGraph.draw", "Microsoft.PowerToys", "videolan.vlc", "Google.chrome", "Rufus.Rufus", "xpipe-io.xpipe", "joplin.joplin", "tailscale.tailscale", "nextcloud.nextclouddesktop", "JanDeDobbeleer.OhMyPosh", "Romanitho.Winget-AutoUpdate", "ONLYOFFICE.DesktopEditors", "AutoHotkey.AutoHotkey", "Apple.iTunes")
 
+$User_DIR = "$User_Dir"
+
 
 ### Functions ###
 ## Install Fonts ##
@@ -29,17 +31,17 @@ function Install-FontsInFolder {
 
 ### Configure Prompt ###
 ## Import Font ##
-Invoke-Webrequest "https://edmi.app/index.php/s/qnyGqr6Zxykgs9Q/download/fonts.zip" -Outfile C:\Users\ryan\Downloads\fonts.zip
-Expand-Archive -LiteralPath C:\Users\ryan\Downloads\fonts.zip -DestinationPath C:\Users\ryan\fonts
+Invoke-Webrequest "https://edmi.app/index.php/s/qnyGqr6Zxykgs9Q/download/fonts.zip" -Outfile "$User_Dir\Downloads\fonts.zip"
+Expand-Archive -LiteralPath $User_Dir\Downloads\fonts.zip -DestinationPath $User_Dir\fonts
 
 # Call the function to install the fonts
 Install-FontsInFolder -folderPath $fontFolderPath
 
 ## Configure Windows Terminal ##
-Invoke-Webrequest "https://edmi.app/index.php/s/MQn4Gb6YarTrX5k/download/settings.json" -Outfile C:\Users\ryan\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+Invoke-Webrequest "https://edmi.app/index.php/s/MQn4Gb6YarTrX5k/download/settings.json" -Outfile "$User_Dir\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings."json
 
 ## Powerhsell Profile Config ##
-Invoke-Webreqeust "https://raw.githubusercontent.com/ryanvanmass/Windows_Setup/main/PowerShell_profile.ps1" -Outfile C:\Users\ryan\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Invoke-Webreqeust "https://raw.githubusercontent.com/ryanvanmass/Windows_Setup/main/PowerShell_profile.ps1" -Outfile "$User_Dir\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
 ### Package Install ###
 Get-ChildItem $Packages | ForEach-Object {
@@ -50,8 +52,8 @@ Get-ChildItem $Packages | ForEach-Object {
 winget install Microsoft.VisualStudioCode --override "/verysilent /suppressmsgboxes /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
 
 # Install Clickpaste
-Invoke-WebRequest "https://github.com/Collective-Software/ClickPaste/releases/download/v1.3.0/ClickPaste_v1.3.0.zip" -OutFile C:\Users\ryan\Downloads\ClickPaste.zip
-Invoke-Webrequest 'https://edmi.app/index.php/s/Mxp4Yrtt3EtwYiG/download/ClickPaste.exe.lnk' -Outfile 'C:\Users\ryan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ClickPaste.exe.lnk'
+Invoke-WebRequest "https://github.com/Collective-Software/ClickPaste/releases/download/v1.3.0/ClickPaste_v1.3.0.zip" -OutFile "$User_Dir\Downloads\ClickPaste.zip"
+Invoke-Webrequest 'https://edmi.app/index.php/s/Mxp4Yrtt3EtwYiG/download/ClickPaste.exe.lnk' -Outfile '$User_Dir\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ClickPaste.exe.lnk'
 
 ## Optioanl Feature Install ##
 # Windows Sandbox
